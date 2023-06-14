74dd24b6a45869860c83d7fa4415281a3c706812
WeltonDev <welton.developer95@gmail.com>
2023-06-14 18:13:56 -0300

    create documentation and fix importUseCase

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
+import swaggerUi from 'swagger-ui-express';
 import express from "express";
 import cors from 'cors'
+
+import swaggerFile from './swagger.json';
 import { routes } from "./routes";
 
 export const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(routes);
